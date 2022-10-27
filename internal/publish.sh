4a8cdd80915b5ef016f37e010a662cd75e49f703
jiangwei <jiangwei@pagoda.com.cn>
2022-10-28 00:18:16 +0800

    config: 发布命令指定源

--- internal/publish.sh
+++ internal/publish.sh
@@ -1,2 +1,2 @@
 cd ./src/lib
-npm publish --access=public && rm ./package.json && rm ./README.md
+npm publish --access=public --registry=https://registry.npmjs.org && rm ./package.json && rm ./README.md
