ca6116301cd8a6bdb6188c2f9d73db6a4f381b9c
jiangwei <jiangwei@pagoda.com.cn>
2022-11-29 10:41:08 +0800

    feat: 输入框补充属性

--- src/packages/components/field/field.vue
+++ src/packages/components/field/field.vue
@@ -13,6 +13,16 @@
         :placeholder="placeholder"
         :placeholder-style="`color:var(--${PREFIX}-number-input-placeholder-color)`"
         :style="{ textAlign: valueAlign }"
+        :maxlength="maxlength"
+        :focus="focus"
+        :confirm-type="confirmType"
+        :confirm-hold="confirmHold"
+        :cursor="cursor"
+        :selection-start="selectionStart"
+        :selection-end="selectionEnd"
+        :adjust-position="adjustPosition"
+        :always-embed="alwaysEmbed"
+        :hold-keyboard="holdKeyboard"
         @input="onInput"
         @blur="emit('blur', $event)"
         @confirm="emit('confirm', $event)"
@@ -24,7 +34,16 @@
         :placeholder="placeholder"
         :placeholder-style="`color:var(--${PREFIX}-number-input-placeholder-color)`"
         :style="{ textAlign: valueAlign, width: '100%' }"
-        auto-height
+        :auto-height="autoHeight"
+        :maxlength="maxlength"
+        :focus="focus"
+        :confirm-type="confirmType"
+        :confirm-hold="confirmHold"
+        :cursor="cursor"
+        :selection-start="selectionStart"
+        :selection-end="selectionEnd"
+        :adjust-position="adjustPosition"
+        :hold-keyboard="holdKeyboard"
         @input="onInput"
         @blur="emit('blur', $event)"
         @confirm="emit('confirm', $event)"
@@ -55,6 +74,28 @@ export interface FieldProps
   disabled?: boolean
   /** 是否只用输入框（主要用于表单情况） */
   onlyInput?: boolean
+  /** 自动高度（textarea生效） */
+  autoHeight?: boolean
+  /** 最大输入长度，设置为 -1 的时候不限制最大长度 */
+  maxlength?: number
+  /** 获取焦点 */
+  focus?: boolean
+  /** 设置键盘右下角按钮的文字，仅在 type="text" 时生效 */
+  confirmType?: string
+  /** 点击键盘右下角按钮时是否保持键盘不收起 */
+  confirmHold?: boolean
+  /** 指定focus时的光标位置 */
+  cursor?: number
+  /** 光标起始位置，自动聚集时有效，需与selection-end搭配使用 */
+  selectionStart?: number
+  /** 光标结束位置，自动聚集时有效，需与selection-start搭配使用 */
+  selectionEnd?: number
+  /** 键盘弹起时，是否自动上推页面 */
+  adjustPosition?: boolean
+  /** 强制 input 处于同层状态，默认 focus 时 input 会切到非同层状态 (仅在 iOS 下生效) */
+  alwaysEmbed?: boolean
+  /** focus时，点击页面的时候不收起键盘 */
+  holdKeyboard?: boolean
 }
 
 withDefaults(defineProps<FieldProps>(), {
@@ -62,6 +103,11 @@ withDefaults(defineProps<FieldProps>(), {
   type: 'text',
   placeholder: '请输入',
   valueAlign: 'right',
+  maxlength: undefined,
+  confirmType: undefined,
+  cursor: undefined,
+  selectionStart: undefined,
+  selectionEnd: undefined,
 })
 
 const emit = defineEmits<{
