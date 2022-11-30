33a034c60bf23e84d9449cc17521b9454e8933ff
jiangwei <jiangwei@pagoda.com.cn>
2022-11-30 15:41:11 +0800

    feat: icon图标更新

--- src/packages/components/icon/demo.vue
+++ src/packages/components/icon/demo.vue
@@ -1,13 +1,15 @@
 <template>
   <DocDemoBlock title="基础用法" card>
-    <view class="grid grid-cols-6 gap-y-30">
+    <view class="grid grid-cols-4">
       <view
         v-for="(name, index) in icons"
         :key="index"
         class="flex flex-col items-center justify-center"
+        style="height: 100px"
+        @tap="onCopy(name)"
       >
         <wei-icon :name="name" size="50" />
-        <text>{{ name }}</text>
+        <text class="text-24">{{ name }}</text>
       </view>
     </view>
   </DocDemoBlock>
@@ -16,13 +18,27 @@
     <wei-icon name="success" size="100" />
     <wei-icon name="success" size="50" color="green" />
   </DocDemoBlock>
+  <wei-safe-bottom />
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
 import DocDemoBlock from '../../doc/doc-demo-block.vue'
+import iconsJSON from '../../fonts/iconfont.json'
 
-const icons = ref<string[]>(['up', 'down', 'left', 'down', 'search', 'close', 'success'])
+const icons = ref<string[]>(iconsJSON.glyphs.map((item) => item.font_class))
+
+const onCopy = (name: string) => {
+  uni.setClipboardData({
+    data: `<wei-icon name="${name}" />`,
+    success: function () {
+      uni.showToast({
+        icon: 'none',
+        title: '复制成功',
+      })
+    },
+  })
+}
 </script>
 
 <style lang="scss" scoped></style>
