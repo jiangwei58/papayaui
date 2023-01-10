c74256110cc20a4d13bec94ec635188c23c6d5e8
jiangwei <jiangwei@pagoda.com.cn>
2023-01-10 11:22:03 +0800

    feat: 新增throttle工具方法

--- src/packages/utils/common.ts
+++ src/packages/utils/common.ts
@@ -68,8 +68,12 @@ export const delay = (timeout: number) => {
   })
 }
 
-/** 防抖 */
-export const debounce = (fn: (...args: any[]) => any, delay = 0) => {
+/**
+ * 防抖
+ * @param fn - 函数
+ * @param wait - 等待时间
+ */
+export const debounce = (fn: (...args: any[]) => any, wait = 0) => {
   let t: number | null = null
   return (...innerArgs: any[]) => {
     if (t !== null) {
@@ -77,6 +81,43 @@ export const debounce = (fn: (...args: any[]) => any, delay = 0) => {
     }
     t = setTimeout(() => {
       fn(...innerArgs)
-    }, delay)
+    }, wait)
+  }
+}
+
+/**
+ * 节流
+ * @param fn - 函数
+ * @param wait - 等待时间
+ * @param options - 配置
+ * @param options.leading - 等待前执行，即立即执行
+ * @param options.trailing - 等待后执行，即超时后执行
+ */
+export const throttle = (
+  fn: (...args: any[]) => any,
+  wait = 0,
+  { leading = false, trailing = false } = {},
+) => {
+  let t: number | null = null
+  let time = new Date()
+  return (...innerArgs: any[]) => {
+    if (leading) {
+      fn(...innerArgs)
+      leading = false
+      return
+    }
+    if (t !== null) {
+      clearTimeout(t)
+    }
+    const endTime = new Date()
+    if (endTime.getTime() - time.getTime() >= wait) {
+      time = endTime
+      fn(...innerArgs)
+    }
+    if (trailing) {
+      t = setTimeout(() => {
+        fn(...innerArgs)
+      }, wait)
+    }
   }
 }
