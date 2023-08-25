0345b3c943891dad0fb845da1f8a479faf19c4dc
jiangwei <jiangwei@pagoda.com.cn>
2023-08-25 14:57:32 +0800

    refactor: update 'lang' util types

--- src/packages/utils/lang.ts
+++ src/packages/utils/lang.ts
@@ -1,77 +1,77 @@
 /**
  * @description: 判断值是否未某个类型
  */
-export const is = <T>(val: T, type: string) => {
+export const is = (val: unknown, type: string) => {
   return Object.prototype.toString.call(val) === `[object ${type}]`
 }
 
 /**
  * @description:  是否为函数
  */
-export const isFunction = (val: unknown) => {
+export const isFunction = (val: unknown): val is (...args: any[]) => any => {
   return is(val, 'Function')
 }
 
 /**
  * @description: 是否为对象
  */
-export const isObject = (val: unknown) => {
+export const isObject = (val: unknown): val is object => {
   return val !== null && is(val, 'Object')
 }
 
 /**
  * @description:  是否为时间
  */
-export const isDate = (val: unknown) => {
+export const isDate = (val: unknown): val is Date => {
   return is(val, 'Date')
 }
 
 /**
  * @description:  是否为undefined类型
  */
-export const isUndefined = (val: unknown) => {
+export const isUndefined = (val: unknown): val is undefined => {
   return is(val, 'Undefined')
 }
 
 /**
  * @description:  是否为数值
  */
-export const isNumber = (val: unknown) => {
+export const isNumber = (val: unknown): val is number => {
   return is(val, 'Number')
 }
 
 /**
  * @description:  是否为promise
  */
-export const isPromise = (val: unknown) => {
+export const isPromise = <T = any>(val: unknown): val is Promise<T> => {
   return val instanceof Promise
 }
 
 /**
  * @description:  是否为字符串
  */
-export const isString = (val: unknown) => {
+export const isString = (val: unknown): val is string => {
   return is(val, 'String')
 }
 
 /**
  * @description:  是否为boolean类型
  */
-export const isBoolean = (val: unknown) => {
+export const isBoolean = (val: unknown): val is boolean => {
   return is(val, 'Boolean')
 }
 
 /**
  * @description:  是否为数组
  */
-export const isArray = (val: unknown) => {
-  return val && Array.isArray(val)
+export const isArray = <T = any>(val: unknown): val is T[] => {
+  return !!val && Array.isArray(val)
 }
 
 /**
  * @description:  是否为普通对象
  */
-export const isPlainObject = (val: unknown) => {
+export const isPlainObject = (val: unknown): val is object => {
   if (!is(val, 'Object')) {
     return false
   }
