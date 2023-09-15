2cd8a636712f534dc0f3106b67e1b131650238b0
jiangwei <jiangwei@pagoda.com.cn>
2023-09-15 16:09:08 +0800

    feat: Split collapse demo

---
+++ src/packages/demos/collapse/demo-4.vue
@@ -0,0 +1,14 @@
+<template>
+  <pa-collapse>
+    <pa-collapse-item>
+      <template #title>
+        <span class="color-primary">标题1</span>
+        <pa-icon name="question" color="var(--pa-color-danger)" />
+      </template>
+      折叠内容折叠内容折叠内容折叠内容
+    </pa-collapse-item>
+    <pa-collapse-item title="标题2" icon="setting" value="内容">
+      折叠内容折叠内容折叠内容折叠内容
+    </pa-collapse-item>
+  </pa-collapse>
+</template>
