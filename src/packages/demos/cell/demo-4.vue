f6ad6faf2eebe64ca300315cb3ac8e0e65089bf2
jiangwei <jiangwei@pagoda.com.cn>
2023-09-15 16:08:03 +0800

    feat: Split cell demo

---
+++ src/packages/demos/cell/demo-4.vue
@@ -0,0 +1,3 @@
+<template>
+  <pa-cell title="单元格" value="内容" suffix="%" />
+</template>
---
+++ src/packages/demos/cell/demo-6.vue
@@ -0,0 +1,12 @@
+<template>
+  <pa-cell value="内容">
+    <template #title>
+      <text>标题</text>
+      <text style="color: red">插槽</text>
+    </template>
+  </pa-cell>
+  <pa-cell title="单元格">
+    <text>内容</text>
+    <text style="color: red">插槽</text>
+  </pa-cell>
+</template>
