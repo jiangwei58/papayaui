f6c8c248ea43d28dad98a732eeb32624bfb0fb28
jiangwei <jiangwei@pagoda.com.cn>
2023-10-10 15:39:19 +0800

    feat: Badge add border and color params

---
+++ src/packages/demos/badge/demo-7.vue
@@ -0,0 +1,10 @@
+<template>
+  <view class="demo-badge">
+    <pa-badge content="6" border>
+      <view class="demo-badge-block"></view>
+    </pa-badge>
+    <pa-badge content="99" border border-color="#000">
+      <view class="demo-badge-block"></view>
+    </pa-badge>
+  </view>
+</template>
