07c057d52e419ed34b2f02e9977f8ec26da56cf4
jiangwei <jiangwei@pagoda.com.cn>
2023-11-07 18:27:49 +0800

    feat: add circle component

---
+++ src/packages/components/circle/props.ts
@@ -0,0 +1,59 @@
+import type { ExtractPropTypes } from 'vue'
+
+export const circleProps = {
+  /**
+   * 目标进度
+   */
+  modelValue: {
+    type: Number,
+    default: 0,
+  },
+  /**
+   * 圆环直径，单位px
+   */
+  size: {
+    type: Number,
+    default: 100,
+  },
+  /**
+   * 进度条颜色
+   */
+  color: {
+    type: String,
+    default: '#009c5d',
+  },
+  /**
+   * 轨道颜色
+   */
+  layerColor: {
+    type: String,
+    default: '#fff',
+  },
+  /**
+   * 文字
+   */
+  text: String,
+  /**
+   * 进度条宽度，单位px
+   */
+  strokeWidth: {
+    type: [Number, String],
+    default: 4,
+  },
+  /**
+   * 是否顺时针
+   */
+  clockwise: {
+    type: Boolean,
+    default: true,
+  },
+  /**
+   * 动画速度（单位为 value/s）
+   */
+  speed: {
+    type: Number,
+    default: 50,
+  },
+}
+
+export type CircleProps = ExtractPropTypes<typeof circleProps>
---
+++ src/packages/demos/circle/demo-2.vue
@@ -0,0 +1,35 @@
+<template>
+  <view>
+    <pa-circle :model-value="progress" stroke-width="8" text="设置宽度" />
+    <pa-circle :model-value="progress" color="#ff2b2b" text="设置颜色" />
+    <pa-circle :model-value="progress" :clockwise="false" text="逆时针" />
+    <pa-circle :model-value="progress" :size="150" text="设置大小" />
+  </view>
+  <view class="mt-15">
+    <pa-button type="primary" @click="add">增加</pa-button>
+    <pa-button type="danger" class="ml-15" @click="subtract">减少</pa-button>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const STEP = 10
+const progress = ref(80)
+
+const add = () => {
+  if (progress.value >= 100) return
+  progress.value += STEP
+}
+
+const subtract = () => {
+  if (progress.value <= 0) return
+  progress.value -= STEP
+}
+</script>
+
+<style lang="scss" scoped>
+:deep(.pa-circle) {
+  margin-right: 15px;
+}
+</style>
