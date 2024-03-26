828c903d4b22fddad1dd2f75c3d33a4236808f41
jiangwei <jiangwei@pagoda.com.cn>
2024-03-26 18:13:25 +0800

    refactor(transition): use wxs to process animations

--- src/packages/components/transition/transition.vue
+++ src/packages/components/transition/transition.vue
@@ -1,136 +1,120 @@
 <template>
   <view
-    v-if="state.inited"
-    :class="[ns.b(), state.classes, customClass]"
+    v-if="inited"
+    :class="[ns.b(), customClass]"
     :style="mergeStyle"
-    @tap="clickHandler"
+    :options="options"
+    :show="show"
+    :change:options="wxs.onOptionsChange"
+    :change:show="wxs.onShowChange"
+    @tap="onClick"
     @touchmove="noop"
   >
     <slot />
   </view>
 </template>
 
-<script lang="ts" setup>
-import { computed, nextTick, reactive, watch } from 'vue'
+<script lang="ts">
+import { computed, defineComponent, reactive, toRefs, watch } from 'vue'
 import useNamespace, { defaultNamespace } from '../../core/useNamespace'
 import { noop } from '../../utils'
 import { transitionEmits, transitionProps } from './props'
 
-const ns = useNamespace('transition')
+export default defineComponent({
+  props: transitionProps,
+  emits: transitionEmits,
+  setup(props, { emit }) {
+    const ns = useNamespace('transition')
 
-const props = defineProps(transitionProps)
-const emit = defineEmits(transitionEmits)
+    const state = reactive<{
+      inited: boolean // 是否显示/隐藏组件
+    }>({
+      inited: false,
+    })
 
-const state = reactive<{
-  inited: boolean // 是否显示/隐藏组件
-  status: '' | 'enter' | 'leave' // 记录组件动画的状态
-  transitionEnded: boolean // 组件是否结束的标记
-  display: boolean // 组件是否展示
-  classes: string // 应用的类名
-}>({
-  inited: false,
-  status: '',
-  transitionEnded: false,
-  display: false,
-  classes: '',
-})
+    const options = computed(() => {
+      return {
+        mainClass: ns.b(),
+        prefix: defaultNamespace,
+        mode: props.mode,
+        duration: +props.duration,
+      }
+    })
 
-const mergeStyle = computed(() => {
-  return {
-    transitionDuration: `${props.duration}ms`,
-    transitionTimingFunction: props.timingFunction,
-    ...props.customStyle,
-  }
-})
+    const mergeStyle = computed(() => {
+      return {
+        transitionTimingFunction: props.timingFunction,
+        ...props.customStyle,
+      }
+    })
 
-// 定义类名，通过给元素动态切换类名，赋予元素一定的css动画样式
-const getClassNames = (name: string) => {
-  const classPrefix = `${defaultNamespace}-${name}`
-  return {
-    enter: `${classPrefix}-enter ${classPrefix}-enter-active`,
-    'enter-to': `${classPrefix}-enter-to ${classPrefix}-enter-active`,
-    leave: `${classPrefix}-leave ${classPrefix}-leave-active`,
-    'leave-to': `${classPrefix}-leave-to ${classPrefix}-leave-active`,
-  }
-}
+    const onInit = async () => {
+      state.inited = true
+    }
 
-const onEnter = async () => {
-  // 动画进入时的类名
-  const classNames = getClassNames(props.mode)
-  // 定义状态和发出动画进入前事件
-  state.status = 'enter'
-  emit('beforeEnter')
-  state.inited = true
-  state.display = true
-  state.classes = classNames.enter
-  await nextTick()
-  await new Promise<void>((resolve) => {
-    return setTimeout(() => {
-      resolve()
-    }, 1000 / 30)
-  })
-  // 标识动画尚未结束
-  emit('enter')
-  state.transitionEnded = false
-  // 组件动画进入后触发的事件
-  emit('afterEnter')
-  // 赋予组件enter-to类名
-  state.classes = classNames['enter-to']
-}
+    const beforeEnter = () => {
+      emit('beforeEnter')
+    }
 
-// 动画离场处理
-const onLeave = () => {
-  // 如果不是展示状态，无需执行逻辑
-  if (!state.display) return
-  const classNames = getClassNames(props.mode)
-  // 标记离开状态和发出事件
-  state.status = 'leave'
-  emit('beforeLeave')
-  // 获得类名
-  state.classes = classNames.leave
+    const enter = () => {
+      emit('enter')
+    }
 
-  nextTick(() => {
-    // 动画正在离场的状态
-    state.transitionEnded = false
-    emit('leave')
-    // 组件执行动画，到了执行的执行时间后，执行一些额外处理
-    setTimeout(onTransitionEnd, +props.duration)
-    state.classes = classNames['leave-to']
-  })
-}
+    const afterEnter = () => {
+      emit('afterEnter')
+    }
 
-// 完成过渡后触发
-const onTransitionEnd = () => {
-  // 如果已经是结束的状态，无需再处理
-  if (state.transitionEnded) return
-  state.transitionEnded = true
-  // 发出组件动画执行后的事件
-  if (state.status === 'leave') {
-    emit('afterLeave')
-  } else {
-    emit('afterEnter')
-  }
-  if (!props.show && state.display) {
-    state.display = false
-    state.inited = false
-  }
-}
+    const beforeLeave = () => {
+      emit('beforeLeave')
+    }
 
-watch(
-  () => props.show,
-  (newVal) => {
-    newVal ? onEnter() : onLeave()
+    const leave = () => {
+      emit('leave')
+    }
+
+    const afterLeave = () => {
+      emit('afterLeave')
+      if (props.destroyOnClose) {
+        state.inited = false
+      }
+    }
+
+    watch(
+      () => props.show,
+      (newVal) => {
+        if (newVal) {
+          onInit()
+        }
+      },
+      {
+        immediate: true,
+      },
+    )
+
+    const onClick = () => {
+      emit('click')
+    }
+
+    return {
+      wxs: {} as any,
+      ns,
+      ...toRefs(state),
+      options,
+      mergeStyle,
+      noop,
+      onClick,
+      beforeEnter,
+      enter,
+      afterEnter,
+      beforeLeave,
+      leave,
+      afterLeave,
+    }
   },
-  {
-    immediate: true,
-  },
-)
-
-const clickHandler = () => {
-  emit('click')
-}
+})
 </script>
+<script module="wxs" lang="wxs" src="./transition.wxs"></script>
 
 <style lang="scss">
-@import './animate.scss';
+@import './transition.scss';
 </style>
