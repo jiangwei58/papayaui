d7d900b6569910b39020e97ae0cf08b1d01b672d
jiangw <jiangw@hotmail.com>
2025-12-06 00:34:23 +0800

    chore: update cos-wx-sdk-v5 to version 1.8.0 in package.json and pnpm-lock.yaml; add fast-xml-parser dependency

--- play/vite.config.ts
+++ play/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni()],
+  build: {
+    watch: {
+      chokidar: {
+        followSymlinks: false,
+        ignored: ['!**/node_modules/papayaui/**'],
+      },
+    },
+  },
 })
